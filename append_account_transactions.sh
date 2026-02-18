93bc9a21df344d6dd8a296d9f360656314c7ed54
Glenn Keßler <7099536+glenn-kessler@users.noreply.github.com>
2026-02-18 16:45:58 +0100

    Update append_account_transactions.sh
    now accounts for ISO-8859-15 encoding in new.csv to be converted to utf-8

--- append_account_transactions.sh
+++ append_account_transactions.sh
@@ -1,15 +1,19 @@
-# appen account transactions to existing ones
+# append account transactions to existing ones
+# assumption: old.csv encoding is utf8
 # create a copy of old.csv (1st line, including 3-byte code for representation of special 'de' characters required by M$ excel)
 # data shaping (could be done in one line)
 # convert date to iso8601 
 # unique-sort and append to x.csv
 
-head -1 old.csv >x.csv
-sed '1d' new.csv   |\
-sed 's/"//g'       |\
-sed 's/\t/ /g'     |\
-sed 's/  */ /g'    |\
-sed 's/\([0-9]\{2\}\)\.\([0-9]\{2\}\)\.\([0-9]\{2\}\)/20\3-\2-\1/g' >u.csv
-sed '1d' old.csv >>u.csv
-sort -r -u u.csv >>x.csv
+
+cat neu.csv                   |\
+iconv -f ISO-8859-15 -t utf-8 |\
+sed '1d'                      |\
+sed 's#"##g'                  |\
+sed 's#\t# #g'                |\
+sed 's#  *# #g'               |\
+sed 's#\([0-9]\{2\}\)\.\([0-9]\{2\}\)\.\([0-9]\{2\}\)#20\3-\2-\1#g' >u.csv
+sed '1d' Umsatz_KSKWN.csv >>u.csv
+head -1  Umsatz_KSKWN.csv  >x.csv
+sort -r -u          u.csv >>x.csv
 rm u.csv
